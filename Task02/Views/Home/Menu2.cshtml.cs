90e3851149c14587034cab7e0536cb5b4c0736a2
Valera <vlaus0@gmail.com>
2023-07-06 20:47:04 +0300

    Добавьте файлы проекта.

---
+++ Task02/Views/Home/Menu2.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Task02.Views.Home
+{
+    public class Menu2Model : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
